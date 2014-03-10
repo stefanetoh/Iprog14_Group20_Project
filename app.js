d5a602303f7cb6aadc7d1f818625fb1a395e6b1b
Carl Ahrjsö <cahrsjo@gmail.com>
2014-03-10 17:12:28 +0100

    Dynamic choose of master+track
    Added comments in the code!

--- app.js
+++ app.js
@@ -3,18 +3,26 @@ var app = angular.module('MyApp', [
 	'myAcademicControllers'
 	]);
 
+/*
+	By: Anna and Calle 2014-03-10
+	This provides routing between different views. 
+	We assign controllers to each view as well.
+
+	OBS! WE MUST ADD CONTROLLERS FOR EACH VIEW!
+*/
+
 app.config(['$routeProvider',
   function($routeProvider) {
 		$routeProvider.
-		when('/partials/:selectedMaster', {
-			templateUrl: 'partials/mainView.html',
+		when('/Master/:selectedMaster/Track/:selectedTrack', {
+			templateUrl: 'views/mainView.html',
 			controller: 'MainController'
 		}).
-		when('/partials', {
-			templateUrl: 'partials/welcomeView.html',
-			controller: 'Controller2'
+		when('/welcome', {
+			templateUrl: 'views/welcomeView.html',
+			controller: 'WelcomeController'
 		}).
 		otherwise({
-			redirectTo: '/partials'
+			redirectTo:'/welcome'
 		});
   }]);
--- js/controllers.js
+++ js/controllers.js
@@ -1,52 +1,92 @@
-/*FRÅN ANGULAR_TUTORIAL: */
+/*
+Calle and Anna 2014-04-10:
+This is where we create all the controllers. 
+Since it's a small project we've done what AngularJS recommend and put all controllers in the same module (myAcademicControllers).
+OBS! WE MUST ADD CONTROLLERS FOR EACH VIEW!
+*/
 
 var myAcademicControllers = angular.module('myAcademicControllers', []);
 
 myAcademicControllers.controller('MainController', ['$scope', '$http', '$routeParams',
   function($scope, $http, $routeParams) {
-  		/*   		FIXA HTTP GET  */
+  		
   		$scope.inputValue = "";
   		$scope.selectedCourse = "DM1021";
-  		$scope.selectedMaster = JSON.parse($routeParams.selectedMaster);  //vi hämtar in det valda master-objektet som en sträng, gör om den till ett JSON-objekt.
+  		$scope.selectedMaster = JSON.parse($routeParams.selectedMaster); // We get the chosen master-object as a string and parses it to a JSON object.
+  		$scope.selectedTrack = $routeParams.selectedTrack;
   		
+  		/*
+	  		TODO: use $http (HTTP GET) to populate $scope.courses with JSON.
+  		*/
   		$scope.courses = [];
   		
+  		/*
+	  		TODO: use $http (HTTP GET) to populate $scope.masters with JSON.
+  		*/
   		$scope.masters = [
 		{
 			name : "Interactive Media Technology",
-			id : 1
+			id : 1,
+			code : "TMETM",
+			tracks : [
+				'META',
+				'METB',
+				'METC',
+				'METD'
+			]
 		},
 		{
 			name : "Human Computer Interaction",
-			id : 2
+			id : 2,
+			code : "THCIM",
+			tracks : ['META']
 		},
 		{
 			name : "Media Management",
-			id : 3
+			id : 3,
+			code : "TMMTM",
+			tracks : ['META']
 		}
 	];
 
 }]);
 
-myAcademicControllers.controller('Controller2', ['$scope', '$http',
+myAcademicControllers.controller('WelcomeController', ['$scope', '$http',
   function($scope, $http) {
 	  	/*   		FIXA HTTP GET  */
 	  	$scope.selectedMaster;
 	  	$scope.understand = "test";
+		$scope.selectedTrack = null;
 		
+		
+		/*
+	  		TODO: use $http (HTTP GET) to populate $scope.masters with JSON.
+  		*/
 		$scope.masters = [
 		{
 			name : "Interactive Media Technology",
-			id : 1
+			id : 1,
+			code : "TMETM",
+			tracks : [
+				'META',
+				'METB',
+				'METC',
+				'METD'
+			]
 		},
 		{
 			name : "Human Computer Interaction",
-			id : 2
+			id : 2,
+			code : "THCIM",
+			tracks : ['META']
 		},
 		{
 			name : "Media Management",
-			id : 3
+			id : 3,
+			code : "TMMTM",
+			tracks : ['META']
 		}
 	];
+
   }]);
   
