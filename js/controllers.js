cff8cdc4d9253efb1516e8fe065cbacb9adc3231
moovin <movin@kth.se>
2014-03-11 12:31:34 +0100

    Nu har vi ändrat om masters-listan att likna vår json-input mer.
    Justerat för detta i koden.

--- js/controllers.js
+++ js/controllers.js
@@ -9,43 +9,132 @@ var myAcademicControllers = angular.module('myAcademicControllers', []);
 
 myAcademicControllers.controller('MainController', ['$scope', '$http', '$routeParams',
   function($scope, $http, $routeParams) {
-  		
-  		$scope.inputValue = "";
-  		$scope.selectedCourse = "DM1021";
-  		$scope.selectedMaster = JSON.parse($routeParams.selectedMaster); // We get the chosen master-object as a string and parses it to a JSON object.
-  		$scope.selectedTrack = $routeParams.selectedTrack;
-  		
-  		/*
+
+		$scope.inputValue = "";
+		$scope.selectedCourse = "DM1021";
+		$scope.selectedMaster = JSON.parse($routeParams.selectedMaster); // We get the chosen master-object as a string and parses it to a JSON object.
+		//$scope.selectedTrack = $routeParams.selectedTrack;
+		$scope.selectedTrack = JSON.parse($routeParams.selectedTrack);
+		console.log("$scope.selectedMaster: " + $scope.selectedMaster + " $scope.selectedTrack: " + $scope.selectedTrack);
+		//$scope.selectedTrack = JSON.parse($scope.selectedTrack);
+		$scope.searchText;
+
+		/*
 	  		TODO: use $http (HTTP GET) to populate $scope.courses with JSON.
   		*/
-  		$scope.courses = [];
-  		
-  		/*
+		$scope.courses = [];
+
+		/*
 	  		TODO: use $http (HTTP GET) to populate $scope.masters with JSON.
   		*/
-  		$scope.masters = [
-		{
-			name : "Interactive Media Technology",
-			id : 1,
-			code : "TMETM",
-			tracks : [
-				'META',
-				'METB',
-				'METC',
-				'METD'
+		$scope.masters = [
+			{
+				name: "Media Technology",
+				id: 1,
+				code: "TMETM",
+				mandatoryCourses: [{
+					code: "DM2573",
+					title: "Sustainabily",
+					level: "second cycle",
+					period: 1,
+					credits: 7.5
+				}, {
+					code: "DM2572",
+					title: "Theory and methodology",
+					level: "second cycle",
+					period: 2,
+					credits: 7.5
+				}],
+				tracks: [
+					{
+						trackCode: "META",
+						title: "Image and Video Technlogy",
+						trackCourses: [{
+							code: "EN2401",
+							title: "Image and video processing",
+							level: "second cycle",
+							period: 2,
+							credits: 6.0
+						}]
+					},
+
+					{
+						trackCode: "METB",
+						title: "Interactive Media Technology",
+						trackCourses: [{
+							code: "DH2641",
+							title: "Interaction Programming",
+							level: "second cycle",
+							period: 3,
+							credits: 6.0
+						}, {
+							code: "DT2141",
+							title: "Multimodal Interaction",
+							level: "second cycle",
+							period: 2,
+							credits: 7.5
+				}, {
+							code: "DD2385",
+							title: "Software engineering",
+							level: "second cycle",
+							period: 1,
+							credits: 6.0
+				}, {
+							code: "DD2395",
+							title: "Computer Security",
+							level: "second cycle",
+							period: 1,
+							credits: 6.0
+				}]
+					},
+
+					{
+						trackCode: "METC",
+						title: "Audio Technlogy",
+						trackCourses: [{
+							code: "DT2300",
+							title: "Sound in interaction",
+							level: "second cycle",
+							period: 1,
+							credits: 7.5
+						}]
+					},
+					{
+						trackCode: "METD",
+						title: "Printed Communication",
+						trackCourses: [{
+							code: "DM2531",
+							title: "Graphic Arts Prduction",
+							level: "second cycle",
+							period: 4,
+							credits: 7.5
+						}]
+					}
 			]
 		},
-		{
-			name : "Human Computer Interaction",
-			id : 2,
-			code : "THCIM",
-			tracks : ['META']
-		},
-		{
-			name : "Media Management",
-			id : 3,
-			code : "TMMTM",
-			tracks : ['META']
+			{
+				name: "Human Computer Interaction",
+				id: 2,
+				code: "THCIM",
+				tracks: [{
+					trackCode: "",
+					title: "",
+					trackCourses: []
+				}],
+				mandatoryCourses: [{
+					code: "DH2610",
+					title: "Theory and methodology for HCI",
+					level: "second cycle",
+					period: 1,
+					credits: 7.5
+				}],
+				conditionalCourses: [{
+					code: "DH2320",
+					title: "Introduction to Visualization and Computer Graphics",
+					level: "second cycle",
+					period: 1,
+					credits: 6.0
+				}]
 		}
 	];
 
@@ -53,40 +142,124 @@ myAcademicControllers.controller('MainController', ['$scope', '$http', '$routePa
 
 myAcademicControllers.controller('WelcomeController', ['$scope', '$http',
   function($scope, $http) {
-	  	/*   		FIXA HTTP GET  */
-	  	$scope.selectedMaster;
-	  	$scope.understand = "test";
+		/*   		FIXA HTTP GET  */
+		$scope.selectedMaster;
+		$scope.understand = "test";
 		$scope.selectedTrack = null;
-		
-		
+
+
 		/*
 	  		TODO: use $http (HTTP GET) to populate $scope.masters with JSON.
   		*/
 		$scope.masters = [
-		{
-			name : "Interactive Media Technology",
-			id : 1,
-			code : "TMETM",
-			tracks : [
-				'META',
-				'METB',
-				'METC',
-				'METD'
+			{
+				name: "Media Technology",
+				id: 1,
+				code: "TMETM",
+				mandatoryCourses: [{
+					code: "DM2573",
+					title: "Sustainabily",
+					level: "second cycle",
+					period: 1,
+					credits: 7.5
+				}, {
+					code: "DM2572",
+					title: "Theory and methodology",
+					level: "second cycle",
+					period: 2,
+					credits: 7.5
+				}],
+				tracks: [
+					{
+						trackCode: "META",
+						title: "Image and Video Technlogy",
+						trackCourses: [{
+							code: "EN2401",
+							title: "Image and video processing",
+							level: "second cycle",
+							period: 2,
+							credits: 6.0
+						}]
+					},
+
+					{
+						trackCode: "METB",
+						title: "Interactive Media Technology",
+						trackCourses: [{
+							code: "DH2641",
+							title: "Interaction Programming",
+							level: "second cycle",
+							period: 3,
+							credits: 6.0
+						}, {
+							code: "DT2141",
+							title: "Multimodal Interaction",
+							level: "second cycle",
+							period: 2,
+							credits: 7.5
+				}, {
+							code: "DD2385",
+							title: "Software engineering",
+							level: "second cycle",
+							period: 1,
+							credits: 6.0
+				}, {
+							code: "DD2395",
+							title: "Computer Security",
+							level: "second cycle",
+							period: 1,
+							credits: 6.0
+				}]
+					},
+
+					{
+						trackCode: "METC",
+						title: "Audio Technlogy",
+						trackCourses: [{
+							code: "DT2300",
+							title: "Sound in interaction",
+							level: "second cycle",
+							period: 1,
+							credits: 7.5
+						}]
+					},
+					{
+						trackCode: "METD",
+						title: "Printed Communication",
+						trackCourses: [{
+							code: "DM2531",
+							title: "Graphic Arts Prduction",
+							level: "second cycle",
+							period: 4,
+							credits: 7.5
+						}]
+					}
 			]
 		},
-		{
-			name : "Human Computer Interaction",
-			id : 2,
-			code : "THCIM",
-			tracks : ['META']
-		},
-		{
-			name : "Media Management",
-			id : 3,
-			code : "TMMTM",
-			tracks : ['META']
+			{
+				name: "Human Computer Interaction",
+				id: 2,
+				code: "THCIM",
+				tracks: [{
+					trackCode: "",
+					title: "",
+					trackCourses: []
+				}],
+				mandatoryCourses: [{
+					code: "DH2610",
+					title: "Theory and methodology for HCI",
+					level: "second cycle",
+					period: 1,
+					credits: 7.5
+				}],
+				conditionalCourses: [{
+					code: "DH2320",
+					title: "Introduction to Visualization and Computer Graphics",
+					level: "second cycle",
+					period: 1,
+					credits: 6.0
+				}]
 		}
 	];
 
   }]);
-  
